b6d68d8f3a313adcaff77c28fd9fa521b1f70b2d
2taeyeop <xoduq0208@naver.com>
2025-07-02 11:31:07 +0900

    feat: 개발환경세팅

---
+++ study/App.js
@@ -0,0 +1,21 @@
+// React 및 React Native에서 제공하는 Core Component(View, Text 등)를 import 해줍니다.
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+// 함수를 통해 사용자 지정 컴포넌트(예제에선 App)를 만들어줍니다.
+// return 값에 화면 구성 요소를 작성합니다.
+const App = () => {
+  return <View style={styles.container}></View>;
+};
+
+// StyleSheet를 사용하여 HTML의 CSS와 유사한 역할을 합니다.
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+});
+
+// 다음과 같이 앱 전체에서 사용하기 위해 JavaScript의 내보내기 기본값으로
+// 사용자 지정 컴포넌트를 내보낼 수 있습니다.
+export default App;
