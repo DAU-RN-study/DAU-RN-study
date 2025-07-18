79ffbe3866ccd5602adbbab0702f1e6b139b687a
E000ng2 <97658607+E000ng2@users.noreply.github.com>
2025-07-18 19:00:01 +0900

    과제 3 : 카카오톡 프로필

--- study/App.js
+++ study/App.js
@@ -1,137 +1,215 @@
-// React 및 React Native에서 제공하는 Core Component(View, Text 등)를 import 해줍니다.
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+    SafeAreaView,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Alert,
+    View,
+    Image
+} from 'react-native';
 
-// 함수를 통해 사용자 지정 컴포넌트(예제에선 App)를 만들어줍니다.
-// return 값에 화면 구성 요소를 작성합니다.
-const App = () => {
-  return (
-   <View style = {styles.container}>
-      {/* 위쪽 */}
-      <View style = {styles.row5container}>
-        {/* 위쪽 왼쪽 */}
-        <View style = {styles.col2container}>
-          {/* 위쪽 왼쪽 위쪽 */}
-          <View style = {styles.row3container}>
-            <View style = {styles.col3container}>
-              <View style = {styles.whi2container}></View>
-              <View style = {styles.blu1container}></View>
-            </View>
-            <View style = {styles.col2container}>
-              <View style = {styles.bla2container}></View>
-            </View>
-          </View>
-          {/* 위쪽 왼쪽 아래쪽 */}
-          <View style = {styles.row2container}>
-            <View style = {styles.whi2container}></View>
-          </View>
-        </View>
+// 아이콘 Image
+import closeIcon from './image/icon1.jpg';
+import giftIcon from './image/icon2.jpg';
+import qrcodeIcon from './image/icon3.jpg';
+import settingIcon from './image/icon4.jpg';
+import myChatIcon from './image/icon5.jpg';
+import editProfileIcon from './image/icon6.jpg';
+import kakaoStoryIcon from './image/icon7.jpg';
+import profileImage from './image/photo.jpg'
+import backgroundImage from './image/back.jpg';
 
-        {/* 위쪽 오른쪽 */}
-        <View style = {styles.col1container}>
-          <View style = {styles.yel2container}></View>
-          <View style = {styles.blu3container}></View>
-        </View>
-      </View>
 
-      {/* 아래쪽 */}
-      <View style = {styles.col2container}>
-
-        {/* 아래 위 */}
-        <View style = {styles.row1container}>
-          <View style = {styles.red1container}></View>
-        </View>
-
-        {/* 아래 아래 */}
-        <View style = {styles.row1container}>
-          <View style = {styles.yel1container}></View>
-          <View style = {styles.bla2container}></View>
-        </View>
-
-      </View>
-   </View>
-  );
+const createalert = () => {
+    Alert.alert(
+        "알림",
+        "버튼을 눌렀습니다.",
+        [
+            {
+                text : "취소",
+                onPress : () => console.log("취소 버튼을 눌렀습니다."),
+                style: "cancel"
+            },
+            {
+                text: "확인",
+                onPress : () => console.log("확인 버튼을 눌렀습니다."),
+            }
+        ]
+    )
 };
 
-// StyleSheet를 사용하여 HTML의 CSS와 유사한 역할을 합니다.
+const App = () => {
+    return (
+        <SafeAreaView style={styles.mainContainer}>
+            {/* 프로필 배경 화면 */}
+            <View style={styles.backgroundLayer}>
+                <Image source={backgroundImage} style={styles.backgroundStyle}/>
+                <View style = {styles.filter} />
+            </View>
+
+            <View style={styles.mainContainer}>
+                {/* 상단 */}
+                <View style={styles.headercontainer}>
+                    {/* 취소 버튼 */}
+                    <View style={styles.headerLeftContainer}>
+                        <TouchableOpacity onPress={createalert}>
+                            <Image source = {closeIcon} style={styles.headerIconStyle}/>
+                        </TouchableOpacity>
+                    </View>
+                    {/* 선물, 큐알, 설정 버튼 */}
+                    <View style={styles.headerRightContainer}>
+                        <TouchableOpacity onPress={createalert}>
+                            <Image source={giftIcon} style={styles.headerIconStyle}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={createalert}>
+                            <Image source={qrcodeIcon} style={styles.headerIconStyle}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={createalert}>
+                            <Image source={settingIcon} style={styles.headerIconStyle}/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                {/* 중앙 */}
+                <View style={styles.profileContainer}>
+                    {/* 프로필 사진 */}
+                    <TouchableOpacity onPress={createalert}>
+                        <Image source={profileImage} style={styles.profileImageStyle}/>
+                    </TouchableOpacity>
+                    {/* 프로필 이름 */}
+                    <Text style={styles.profileNameText}>조예영</Text>
+                    {/* 구분자 */}
+                    <View style={{backgroundColor : 'gray', height : 1, width : '100%', marginTop : 25}}/>
+                </View>
+
+                {/* 하단 */}
+                <View style={styles.bottomcontainer}>
+                    {/* 나와의 채팅 */}
+                    <TouchableOpacity onPress={createalert} style={styles.bottomMenuItem}>
+                        <Image source={myChatIcon} style={styles.bottomIconStyle}/>
+                        <Text style={styles.bottomItemText}>나와의 채팅</Text>
+                    </TouchableOpacity>
+                    {/* 프로필 편집 */}
+                    <TouchableOpacity onPress={createalert} style={styles.bottomMenuItem}>
+                        <Image source={editProfileIcon} style={styles.bottomIconStyle}/>
+                        <Text style={styles.bottomItemText}>프로필 편집</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={createalert} style={styles.bottomMenuItem}>
+                        <Image source={kakaoStoryIcon} style={styles.bottomIconStyle}/>                    
+                        <Text style={styles.bottomItemText}>카카오스토리</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+
+)};
+
 const styles = StyleSheet.create({
-  container : {
-    flex : 1,
-  },
+    mainContainer : {
+        flex : 1,
+    },
 
-  row1container : {
-    flex : 1,
-    flexDirection : 'row',
-  },
+    backgroundLayer : {
+        position : 'absolute',
+        alignItems : 'center',
+        height : '100%',
+        width : '100%',
+    },
 
-  row2container : {
-    flex : 2, 
-    flexDirection : 'row',
-  },
+    backgroundStyle : {
+        resizeMode : 'cover',
+        position : 'absolute',
+        height : '100%',
+        width : '100%',
+    },
 
-  row3container : {
-    flex : 3, 
-    flexDirection : 'row',
-  },
+    filter : {
+        height : '100%',
+        width : '100%',
+        backgroundColor : 'rgba(128,128,128,0.2)',
+        position : 'absolute',
+    },
 
-  row5container : {
-    flex : 5, 
-    flexDirection : 'row',
-  },
+    headercontainer : {
+        flex : 1,
+        // backgroundColor : 'yellow',
+        flexDirection : 'row',
+        alignItems : 'center',
+        marginHorizontal : 10,
+    },
 
-  col1container : {
-    flex : 1,
-    flexDirection : 'column'
-  },
+    headerLeftContainer : {
+        flex : 1,
+        // backgroundColor : 'pink',
+        flexDirection : 'row',
+        justifyContent : 'flex-start',
+    },
 
-  col2container : {
-    flex : 2,
-    flexDirection : 'column',
-  },
+    headerRightContainer : {
+        flex : 1,
+        // backgroundColor : 'orange',
+        flexDirection : 'row',
+        justifyContent : 'flex-end',
+    },
 
-  col3container : {
-    flex : 3,
-    flexDirection : 'column',
-  },
+    headerIconStyle : {
+        height : 20,
+        width : 20,
+        tintColor : 'white',
+        resizeMode : 'contain',
+        marginHorizontal : 10,
+    },
 
-  red1container : {
-    flex : 1,
-    backgroundColor : 'red',
-  },
+    profileContainer : {
+        flex : 12,
+        // backgroundColor : 'green',
+        flexDirect : 'column',
+        alignItems : 'center',
+        justifyContent : 'flex-end'
+    },
 
-  yel1container : {
-    flex : 1,
-    backgroundColor : 'yellow',
-  },
+    profileImageStyle : {
+        height : 100,
+        width : 100,
+        resizeMode : 'cover', // 채우기
+        borderRadius : 30, // 모서리
+    },
 
-  yel2container : {
-    flex : 2,
-    backgroundColor : 'yellow',
-  },
+    profileNameText : {
+        fontSize : 18, 
+        color : 'white',
+        marginVertical : 15,
+    },
 
-  whi2container : {
-    flex : 2,
-    backgroundColor : 'white',
-  },
+    bottomcontainer : {
+        flex : 2.5,
+        // backgroundColor : 'blue',
+        flexDirection : 'row',
+        justifyContent : 'space-between',
+    },
 
-  blu1container : {
-    flex : 1,
-    backgroundColor : 'blue',
-  },
+    bottomMenuItem : {
+        flex : 1,
+        // backgroundColor : 'powderblue',
+        flexDirextion : 'cloumn',
+        alignItems : 'center',
+        justifyContent : 'center',
+    },
 
-  blu3container : {
-    flex : 3,
-    backgroundColor : 'blue',
-  },
-
-  bla2container : {
-    flex : 2,
-    backgroundColor : 'black',
-  },
-
+    bottomIconStyle : {
+        height : 25,
+        width : 25,
+        tintColor : 'white',
+        resizeMode : 'contain',
+    },
+        
+    bottomItemText : {
+        fontSize : 15,
+        color : 'white',
+        marginVertical : 10,
+    },
 
 });
 
-// 다음과 같이 앱 전체에서 사용하기 위해 JavaScript의 내보내기 기본값으로
-// 사용자 지정 컴포넌트를 내보낼 수 있습니다.
 export default App;
