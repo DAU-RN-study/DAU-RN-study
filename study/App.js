88cb48247da6b47b6d1bc1c3230459376f687416
Taemin <tmysds102@gmail.com>
2025-07-22 12:15:55 +0900

    feat: 3주차 과제

--- study/App.js
+++ study/App.js
@@ -1,17 +1,45 @@
-// React 및 React Native에서 제공하는 Core Component(View, Text 등)를 import 해줍니다.
 import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView, ImageBackground,Image, TouchableOpacity, Alert} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Image, Alert, ScrollView, SafeAreaView, FlatList, Linking} from 'react-native';
 
-import xLogo from './img/xLogo.png'
-import giftLogo from './img/giftLogo.png'
-import qrLogo from './img/qrLogo.png'
+import cahtplusLogo from './img/chatplusLogo.png'
+import magnifierLogo from './img/magnifierLogo.png'
+import musicLogo from './img/musicLogo.png'
 import setLogo from './img/setLogo.png'
-import pi from './img/pi.jpg'
-import chatLogo from './img/chatLogo.png'
-import penLogo from './img/penLogo.png'
-import quoteLogo from './img/quoteLogo.png'
-import backgroundimg from './img/backgroundimg.jpg'
+import kakaotalknormal from './img/kakaotalknormal.jpg'
 
+import {DATA} from './DATA/DATA'
+
+const url = "https://www.naver.com"
+
+const Item = ({ title, description, data }) => (
+  <TouchableOpacity onPress={() => createchatAlert(title)}>
+    <View style={styles.item}>
+      <Image source={kakaotalknormal} style={styles.kakaotalknormal}></Image>
+      <View style={styles.CC1}>
+          <View style={styles.CC2}>
+            <View style={styles.TC}>
+              <Text style={styles.title}>{title}</Text>
+              </View>
+            <View style={styles.DC}>
+              <Text style={styles.data}>{data}</Text>
+            </View>
+          </View>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+
+const createchatAlert=( title )=>{
+  Alert.alert(
+    title,
+    title + " 님과의 채팅방입니다.",
+    [
+      {text: "확인", onPress:()=>console.log("확인 버튼을 눌렀습니다")},
+      {text: "취소",style: "cancel",onPress:()=> console.log("취소 버튼을 눌렀습니다")}
+    ]
+  )
+}
 
 const createAlert=( name )=>{
   Alert.alert(
@@ -23,202 +51,189 @@ const createAlert=( name )=>{
     ]
   )
 }
-// 함수를 통해 사용자 지정 컴포넌트(예제에선 App)를 만들어줍니다.
-// return 값에 화면 구성 요소를 작성합니다.
+
 const App = () => {
   return <SafeAreaView style={styles.container} >
-    <ImageBackground source={backgroundimg} style={styles.backgroundimg} imageStyle={{ opacity: 0.75 }}>
-    
-
-    <View style={styles.Top}>
-
-      <View style={styles.TopLeft}>
-
-        <TouchableOpacity onPress={() => createAlert('나가기')}>
-          <Image source={xLogo} style={styles.xLogo}></Image>
+    <View style={styles.top}>
+      <View style={styles.chating}><Text style={styles.CF}>채팅</Text></View>
+      <View style={styles.icon}>
+        <TouchableOpacity onPress={() => createAlert('검색')}>
+          <Image source={magnifierLogo} style={styles.magnifierLogo}></Image>
         </TouchableOpacity>
 
-      </View>
-
-      <View style={styles.TopRight}>
-
-        <TouchableOpacity onPress={() => createAlert('선물')}>
-          <Image source={giftLogo} style={styles.giftLogo}></Image>
-        </TouchableOpacity>
-          
-        <TouchableOpacity onPress={() => createAlert('QR코드')}>
-          <Image source={qrLogo} style={styles.qrLogo}></Image>
+        <TouchableOpacity onPress={() => createAlert('오픈채팅')}>
+          <Image source={cahtplusLogo} style={styles.cahtplusLogo}></Image>
         </TouchableOpacity>
 
+        <TouchableOpacity onPress={() => createAlert('음악')}>
+          <Image source={musicLogo} style={styles.musicLogo}></Image>
+        </TouchableOpacity>
+        
         <TouchableOpacity onPress={() => createAlert('설정')}>
           <Image source={setLogo} style={styles.setLogo}></Image>
         </TouchableOpacity>
       </View>
-
     </View>
 
+    <ScrollView style={styles.C2}>
 
-
-    <View style={styles.Main}>
-
-      <View style={styles.MainTop}>
-        <TouchableOpacity onPress={() => createAlert('프로필')}>
-          <Image source={pi} style={styles.pi}></Image>
+      <View style={styles.ad}>
+        <TouchableOpacity onPress={() => Linking.openURL(url)} style={styles.AD1}>
+          <Text style={styles.ADF}>광고 배너</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.MainBottom}>
-        <TouchableOpacity onPress={() => createAlert('이름')}>
-          <Text style={styles.Name}>여태민</Text>
-        </TouchableOpacity>
+
+      <View style={styles.chat}>
+        <FlatList
+        data={DATA.page}
+        renderItem={({ item }) => (<Item title={item.title} description={item.description} data={item.date} />)} 
+        keyExtractor={item => item.id.toString()}
+        />
       </View>
 
-    </View>
+    </ScrollView>
+
+
     
 
     
-    <View style={styles.Bottom}>
-
-      <View style={styles.Botcontainer}>
-        <TouchableOpacity onPress={() => createAlert('나와의 채팅')}>
-        <Image source={chatLogo} style={styles.chatLogo}></Image>
-      </TouchableOpacity> 
-      <TouchableOpacity onPress={() => createAlert('나와의 채팅 글자')}> 
-        <Text style={styles.t1}>나와의 채팅</Text>
-      </TouchableOpacity>
-      </View>
-
-      <View style={styles.Botcontainer}>
-        <TouchableOpacity onPress={() => createAlert('프로필 편집')}>
-        <Image source={penLogo} style={styles.penLogo}></Image>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => createAlert('프로필 편집 글자')}>
-        <Text style={styles.t1}>프로필 편집</Text>
-      </TouchableOpacity>
-      </View>
-
-      <View style={styles.Botcontainer}>
-        <TouchableOpacity onPress={() => createAlert('카카오스토리')}>
-        <Image source={quoteLogo} style={styles.quoteLogo}></Image>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => createAlert('카카오스토리 글자')}>
-        <Text style={styles.t1}>카카오스토리</Text>
-      </TouchableOpacity>
-      </View> 
-
-    </View>
-    </ImageBackground>
   </SafeAreaView>;
 };
 
-// StyleSheet를 사용하여 HTML의 CSS와 유사한 역할을 합니다.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
   },
 
-  backgroundimg:{
-    width: 412,
-    height: 840,
-  },
-
-  Top: {
-    flex: 8,
+  top: {
+    flex: 0,
+    backgroundColor: 'white',
     flexDirection: 'row',
   },
-  
-  TopLeft:{
-    flex: 10,
+
+  chating: {
+    flex: 6,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    margin: 5,
   },
 
-  xLogo:{
-    height: 20,
-    width:20,
-    margin: 10,
+  CF:{
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 
-  TopRight:{
+  icon: {
     flex: 4,
+    backgroundColor: 'white',
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
-  giftLogo:{
-    height: 20,
-    width:20,
-    margin: 10,
+  magnifierLogo:{
+    width: 27,
+    height: 27,
+    margin: 7,
   },
 
-  qrLogo:{
-    height: 20,
-    width:20,
-    margin: 10,
+  cahtplusLogo: {
+    width: 27,
+    height: 27,
+    margin: 7
   },
 
-  setLogo:{
-    height: 20,
-    width:20,
-    margin: 10,
+  musicLogo: {
+    width: 27,
+    height: 27,
+    margin: 7,
   },
 
-  Main: {
+  setLogo: {
+    width: 27,
+    height: 27,
+    margin: 7,
+  },
+
+  C2: {
+    flex:1,
+    backgroundColor: 'white',
+  },
+
+  ad: {
     flex: 2,
+    backgroundColor: 'white',
     alignItems: 'center',
-    borderBottomColor : 'black',
-    borderBottomWidth : 0.5
+    justifyContent: 'center',
   },
 
-  pi:{
-    width:100,
-    height:100,
-    borderRadius:25,
+  AD1: {
+    borderRadius: 10,
+    height: 70,
+    width: 400,
+    backgroundColor: '#E2E2E2',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
-  Name:{
+  ADF: {
+    fontSize: 25,
     fontWeight: 'bold',
-    margin:20,
-    color: 'white',
   },
 
-  Bottom: {
-    flex: 1.75,
+  chat: {
+    flex: 7,
+    backgroundColor: 'white',
+  },
+
+  item:{
+    flexDirection: 'row',
+    margin: 10,
+
+  },
+
+  kakaotalknormal: {
+    borderRadius: 15,
+    height: 60,
+    width: 60,
+  },
+
+  CC1: {
+    flexDirection: 'colum',
+    margin: 10,
+  },
+
+  CC2: {
     flexDirection: 'row',
   },
 
-  Botcontainer:{
-    flex: 3,
-    margin: 15,
-    flexDirection: 'column',
+  TC:{
+    paddingRight: 220,
+
+  },
+
+  DC:{
+    width: 60,
+    justifyContent: 'center',
     alignItems: 'center',
   },
 
-  chatLogo: {
-    width: 40,
-    height:40,
-    margin: 20,
-    marginTop:5,
+  title: {
+  fontSize: 18,
+  fontWeight: 'bold',
   },
 
-  penLogo: {
-    width: 40,
-    height:40,
-    margin: 20,
-    marginTop:5,
+  description: {
+    fontSize: 10,
   },
 
-  quoteLogo: {
-    width: 40,
-    height:40,
-    margin: 20,
-    marginTop:5,
-  },
-
-  t1: {
-    color: 'white',
-    fontWeight: 'bold',
+  data: {
+    fontSize: 14,
+    color: 'gray',
   }
+
 });
 
-
-// 다음과 같이 앱 전체에서 사용하기 위해 JavaScript의 내보내기 기본값으로
-// 사용자 지정 컴포넌트를 내보낼 수 있습니다.
 export default App;
