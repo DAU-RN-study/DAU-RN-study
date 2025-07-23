983b9e762a6f95c5963d1ee85911503c6d2fdafc
somin1116 <sominjung1116@gmail.com>
2025-07-24 03:34:36 +0900

    2차 과제

--- study/App.js
+++ study/App.js
@@ -1,119 +1,197 @@
 import React from 'react'
 import {
 	View,
-  Text,
 	StyleSheet,
 	ScrollView,
-	Image
+	Image,
+	SafeAreaView,
+    TouchableOpacity,
+    Text,
+	ImageBackground,
+    Alert
 } from 'react-native'
 
+import bg from './image/bg.png'
+import chat from './image/chat.png'
+import edit from './image/edit.png'
+import exit from './image/exit.png'
+import pro from './image/profile.png'
+import gift from './image/gift.png'
+import qrcode from './image/qrcode.png'
+import setting from './image/setting.png'
+
+
+
+
+
+
+const alertbutton = (message) => {
+	Alert.alert(
+		'알림',
+		message,
+		[{text: '취소', 
+			onPress: () => console.log('취소됨'),
+			style: 'cancel'
+		},
+	    {text: '확인',
+			onPress: () => console.log('확인됨')}]
+	)
+}
 
 const App = () => {
 	return (
-		<View style={styles.container}>
-			<View style={styles.first}>
-				<View style={styles.yblue}>
-					<View style={styles.yel}></View>
-					<View style={styles.blu}></View>
-				</View>
-				<View style={styles.wbb}>
-					<View style={styles.whi}></View>
-					<View style={styles.bb}>
-						<View style={styles.wb}>
-							<View style={styles.white}></View>
-							<View style={styles.blue}></View>
-						</View>
-						<View style={styles.bla}></View>
+		<SafeAreaView style={styles.safearea}>
+			<ImageBackground source={bg}
+			style={styles.bg}>
+				<View style={styles.overlay}></View>
+				<View style={styles.upthings}>
+					<TouchableOpacity style={styles.exiticon}
+				    onPress={() => alertbutton('나가기 버튼입니다')}>
+					    <Image source={exit} style={styles.exit}></Image>
+				    </TouchableOpacity>
+					<View style={styles.rightthings}>
+						<TouchableOpacity style={styles.servgift}
+				        onPress={() => alertbutton('선물 버튼입니다')}>
+					        <Image source={gift} style={styles.gift}></Image>
+				        </TouchableOpacity>
+					    <TouchableOpacity style={styles.qr}
+				        onPress={() => alertbutton('QR코드 버튼입니다')}>
+					        <Image source={qrcode} style={styles.qrcode}></Image>
+				        </TouchableOpacity>
+					    <TouchableOpacity style={styles.settings}
+				        onPress={() => alertbutton('설정 버튼입니다')}>
+					        <Image source={setting} style={styles.setting}></Image>
+				        </TouchableOpacity>
 					</View>
 				</View>
-			</View>
-			<View style={styles.second}>
-				<View style={styles.red}></View>
-				<View style={styles.yb}>
-					<View style={styles.yellow}></View>
-					<View style={styles.black}></View>
+				<TouchableOpacity style={styles.profile}
+				onPress={() => alertbutton('프로필입니다')}>
+					<Image source={pro} style={styles.pro}></Image>
+					<Text style={styles.name}>정소민</Text>
+				</TouchableOpacity>
+				<View style={styles.seperate}></View>
+				<View style={styles.things}>
+				    <TouchableOpacity style={styles.chatting}
+				    onPress={() => alertbutton('채팅 버튼입니다')}>
+					    <Image source={chat} style={styles.chat}></Image>
+					    <Text style={styles.mychatting}>나와의 채팅</Text>
+				    </TouchableOpacity>
+					<TouchableOpacity style={styles.editprofile}
+				    onPress={() => alertbutton('프로필 편집 버튼입니다')}>
+					    <Image source={edit} style={styles.edit}></Image>
+					    <Text style={styles.proflieedit}>프로필 편집</Text>
+				    </TouchableOpacity>
 				</View>
-			</View>
-		</View>
+			</ImageBackground>
+		</SafeAreaView>
 	)
 }
 
 const styles = StyleSheet.create({
-	container:{
-		backgroundColor: "red",
-		flex: 1,
-		flexDirection: 'column',
+	safearea:{
+		flex:1,
 	},
-	first:{
-		backgroundColor: "white",
-		flex: 6,
-		flexDirection: 'row-reverse',
+	bg:{
+		flex:1,
+		width: '100%', 
+		height: '100%'
 	},
-	yblue:{
-		backgroundColor: "blue",
-		flex: 1,
-		flexDirection: 'column',
+	overlay:{
+		position:'absolute',
+		top: 0, left: 0, right: 0, bottom: 0,
+		backgroundColor: 'rgba(0,0,0,0.4)',
 	},
-	yel:{
-		backgroundColor: "yellow",
-		flex: 1,
+	profile:{
+		alignItems:'center',
+		marginTop: 500,
 	},
-	blu:{
-		backgroundColor: "blue",
-		flex: 2,
+	pro:{
+		width:100,
+		height:100,
+		borderRadius:30,
 	},
-	wbb:{
-		backgroundColor: "white",
-		flex: 2,
-		flexDirection: 'column-reverse',
+	name:{
+		marginTop: 10,
+		fontWeight: 'bold',
+		fontSize: 18,
+		color: '#ffffffff'
 	},
-	whi:{
-		backgroundColor: "white",
-		flex: 1,
+	seperate:{
+		width: '100%',
+		height: 0.5,
+		backgroundColor: '#fff',
+		alignSelf: 'center',
+		marginVertical: 30,
 	},
-	bb:{
-		backgroundColor: "black",
-		flex: 1,
+	things:{
+		flexDirection:'row',
+	},
+	chatting:{
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginBottom: 10,
+		flex:1,
+	},
+	chat:{
+		width: 25,
+		height: 25,
+	},
+	editprofile:{
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginBottom: 10,
+		flex:1,
+	},
+	edit:{
+		width:30,
+		height:30,
+	},
+	mychatting:{
+		color: '#fff',
+		marginTop: 10,
+		fontSize: 15
+	},
+	proflieedit:{
+		color: '#fff',
+		marginTop: 10,
+		fontSize: 15
+	},
+	upthings:{
 		flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 20,
+        marginHorizontal: 18,
 	},
-	wb:{
-		backgroundColor: "white",
-		flex: 1,
-		flexDirection: 'column',
+	rightthings:{
+		flexDirection:'row',
+	    alignItems: 'center',
 	},
-	white:{
-		backgroundColor: "white",
-		flex: 2,
+	exit:{
+		width:25,
+		height:25,
 	},
-	blue:{
-		backgroundColor: "blue",
-		flex: 1,
+	gift:{
+		width:25,
+		height:25,
 	},
-	bla:{
-		backgroundColor: "black",
-		flex: 0.9,
+	qrcode:{
+		width:25,
+		height:25,
 	},
-	second:{
-		backgroundColor: "red",
-		flex: 2,
+	setting:{
+		width:25,
+		height:25,
 	},
-	red:{
-		backgroundColor: "red",
-		flex: 1,
+	servgift:{
+		marginRight: 15,
 	},
-	yb:{
-		backgroundColor: "yellow",
-		flex: 1,
-		flexDirection : "row"
+	qr: {
+		marginRight: 15,
 	},
-	yellow:{
-		backgroundColor: "yellow",
-		flex: 1,
-	},
-	black:{
-		backgroundColor: "black",
-		flex: 2,
-	},
+	settings: {
+		marginRight: 0,
+	}
 })
 
 export default App
