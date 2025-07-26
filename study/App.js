4df29c8f7b943e5cf82a47eceb139a4d0a64d489
JangYuSeung <162396042+JangYuSeung@users.noreply.github.com>
2025-07-26 18:48:45 +0900

    1주차 실습과제

--- study/App.js
+++ study/App.js
@@ -3,72 +3,108 @@ import { View, StyleSheet, Text } from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container0}>
+    <View style={styles.c0}>
       
-      {/* 상단: Powderblue 영역 */}
       <View style={styles.c1}>
-        <Text>Powderblue</Text>
+        <View style={styles.c11}></View>
+        <View style={styles.c12}></View>
+        <View style={styles.c13}></View>
       </View>
-      
-      {/* 하단: 좌우 1:1 가로 분할 */}
+
       <View style={styles.c2}>
-        
-        {/* 왼쪽: 세로 1:2 분할 */}
-        <View style={styles.c3}>
-          <View style={styles.c5}>
-            <Text>Yellow</Text>
-          </View>
-          <View style={styles.c6}>
-            <Text>Lavender</Text>
-          </View>
+        <View style={styles.c21}>
+          <View style={styles.c211}></View>
+          <View style={styles.c212}></View>
+          <View style={styles.c213}></View>
         </View>
-        
-        {/* 오른쪽: Pink 전체 */}
-        <View style={styles.c4}>
-          <Text>Pink</Text>
+        <View style={styles.c22}>
+          <View style={styles.c221}></View>
+          <View style={styles.c222}></View>
         </View>
-      
+      </View>
+
+      <View style={styles.c3}>
+        <View style={styles.c31}></View>
+        <View style={styles.c32}>
+          <View style={styles.c321}></View>
+          <View style={styles.c322}></View>
+        </View>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container0: {
-    flex: 1,
-    flexDirection: 'column'
+  c0:{
+    flex:1,
+    flexDirection:'column'
   },
-  c1: {
-    flex: 1,
-    backgroundColor: 'powderblue',
-    justifyContent: 'center',
-    alignItems: 'center'
+  c1:{
+    flex:1,
+    flexDirection:'row'
   },
-  c2: {
-    flex: 3,
-    flexDirection: 'row'
+    c2:{
+    flex:2,
+    flexDirection:'column'
   },
-  c3: {
-    flex: 1,
-    flexDirection: 'column'
+  c3:{
+    flex:1,
+    flexDirection:'column'
   },
-  c4: {
-    flex: 1,
-    backgroundColor: 'pink',
-    justifyContent: 'center',
-    alignItems: 'center'
+  c11:{
+    flex:1.5,
+    backgroundColor:'white'
   },
-  c5: {
-    flex: 1,
-    backgroundColor: 'yellow',
-    justifyContent: 'center',
-    alignItems: 'center'
+  c12:{
+    flex:1,
+    backgroundColor:'black'
   },
-  c6: {
-    flex: 2,
-    backgroundColor: 'lavender',
-    justifyContent: 'center',
-    alignItems: 'center'
+  c13:{
+    flex:1.25,
+    backgroundColor:'yellow'
+  },
+  c21:{
+    flex:1,
+    flexDirection:'row'
+  },
+  c211:{
+    flex:1.5,
+    backgroundColor:'blue'
+  },
+  c212:{
+    flex:1,
+    backgroundColor:'black'
+  },
+  c213:{
+    flex:1.25,
+    backgroundColor:'blue'
+  },
+  c22:{
+    flex:2,
+    flexDirection:'row'
+  },
+  c221:{
+    flex:2
+  },
+  c222:{
+    flex:1,
+    backgroundColor:'blue'
+  },
+  c31:{
+    flex:1,
+    backgroundColor:'red'
+  },
+  c32:{
+    flex:1,
+    flexDirection:'row'
+  },
+  c321:{
+    flex:1,
+    backgroundColor:'yellow'
+  },
+  c322:{
+    flex:2,
+    backgroundColor:'black'
   }
 });
 
